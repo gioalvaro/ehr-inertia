7467b7de51d38a54e1c185ace6a358bbec372a64
Alvaro Fraga <gioalvaro@gmail.com>
2020-10-04 02:43:39 -0300

    add: modules to store

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,10 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import PatientModule from './modules/PatientModule';
+import LaboratoryModule from './modules/LaboratoryModule';
+import NursingNoteModule from './modules/NursingNoteModule';
+import PhysicianNoteModule from './modules/PhysicianNoteModule';
+import MedicationModule from './modules/MedicationModule';
+import MedicalRecordModule from './modules/MedicalRecordModule';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        patient: PatientModule        
+        patient: PatientModule,
+        medicalRecord: MedicalRecordModule,
+        medication: MedicationModule,
+        nursingNote: NursingNoteModule,
+        physicianNote: PhysicianNoteModule,
+        laboratory: LaboratoryModule        
     }
 });
